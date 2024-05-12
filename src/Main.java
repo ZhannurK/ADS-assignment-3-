444849b57e7454bb01bbf16cd6e4b6767486e198
Zhannur Kainedin <230030@astanait.edu.kz>
2024-05-12 18:12:22 +0500

    started main

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,18 @@
 public class Main {
 
     public static void main(String[] args) {
-
+        testBST();
     }
 
+    public static void testMyHashTable(){}
+
+    public static void testBST(){
+        BST<String, Double> productPrices  = new BST<>();
+        productPrices .put("Apple", 0.99);
+        productPrices.put("Banana", 0.59);
+        productPrices.put("Orange", 1.29);
+        productPrices.put("Watermelon", 1.59);
+
+
+    }
 }
